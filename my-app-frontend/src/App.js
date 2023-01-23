b80fbc679a4939d71337de177b40b6d4691acae1
Hassan Razak <hassanrazak1991@gmail.com>
2023-01-23 17:53:02 -0500

    added cart component

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Home />
+      <Cart />
       <ReceipePage />
 
     </div>
---
+++ my-app-frontend/src/Cart.jsx
@@ -0,0 +1,7 @@
+const Cart = () => {
+    return ( 
+        <div>user cart that holds a list of all grocery items</div>
+     );
+}
+ 
+export default Cart;
