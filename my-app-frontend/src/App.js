f78978970438681d6db5ddaa7b9d9f633e4877e9
Hassan Razak <hassanrazak1991@gmail.com>
2023-01-23 17:47:09 -0500

    added reciepe page component

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,8 +1,11 @@
 import Home from "./Home";
+import ReceipePage from "./ReceipePage";
 
 function App() {
   return (
     <div className="App">
+      <Home />
+      <ReceipePage />
 
     </div>
   );
---
+++ my-app-frontend/src/ReceipePage.jsx
@@ -0,0 +1,7 @@
+const ReceipePage = () => {
+    return ( 
+        <div>takes in all of the receipes and sends them down</div>
+     );
+}
+ 
+export default ReceipePage;
