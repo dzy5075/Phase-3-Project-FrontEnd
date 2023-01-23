1c80b1c23b12777bac13ff99fcd08ba4ab9ea8c5
Hassan Razak <hassanrazak1991@gmail.com>
2023-01-23 17:50:08 -0500

    added recipes

--- my-app-frontend/src/ReceipePage.jsx
+++ my-app-frontend/src/ReceipePage.jsx
@@ -1,6 +1,11 @@
+import Receipes from "./Recipes";
+
 const ReceipePage = () => {
     return ( 
+        <>
         <div>takes in all of the receipes and sends them down</div>
+        <Receipes />
+        </>
      );
 }
  
---
+++ my-app-frontend/src/Recipes.jsx
@@ -0,0 +1,7 @@
+function Receipes(){
+    return(
+        <div>show casing all of the recipes. Have image tag, and paragraph to descrip recipe. Will have multiple cards</div>
+    )
+}
+
+export default Receipes
