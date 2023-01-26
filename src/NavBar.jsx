715a5df86980401a7a1163a828f2c6ee840ced78
Daniel Yim <daniel.yim96@gmail.com>
2023-01-26 17:53:49 -0500

    nav bar button css

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -10,18 +10,20 @@ function NavBar ({search,setSearch}){
             <div className = "banner">
                 <div className = "logo">
                     <img onClick={() => {navigate('/')}} src="/QuickCart Logo.png" alt="QuickCartLogo" href="/"/> 
-                </div>
-                <div classname = "cartlogo">
-                <button onClick={() => {navigate('/Cart')}} className = "Cart"> Cart </button>
-                
-                
-                <button onClick={(e) => {
-                    console.log("hello!")
-                    fetch("http://localhost:9292/cart_items", {
-                        method: "DELETE",
-                    })
-                }}
-                className = "buttons"> New Cart </button>
+                    <div className='right-btns'>
+                        <button onClick={() => {navigate('/Cart')}} className = "Cart"> Cart </button>
+                        
+                        
+                        <button onClick={(e) => {
+                            console.log("hello!")
+                            fetch("http://localhost:9292/cart_items", {
+                                method: "DELETE",
+                            })
+                        }}
+                    > New Cart </button>
+
+                    </div>
+                   
                 </div>
             </div>
         </nav>
